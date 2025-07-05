1693e74b451b4e6b4f1afb280c0154a518436d78
Your Name <you@example.com>
2025-07-04 21:01:59 -0300

    teste1

---
+++ exemplos.js
@@ -0,0 +1,18 @@
+const result=document.querySelector(".result")
+
+const criaItem= (elemHtml, atrib, valor) => {
+    const obj= document.createElement(elemHtml)
+    obj[atrib]=valor
+    return obj
+}
+
+receitas.forEach( function(receita) {
+    result.appendChild(criaItem("h3", "textContent", receita.nome))
+    result.appendChild(criaItem("p", "textContent", receita.descricao))
+    result.appendChild(criaItem("img" , "src", receita.foto))
+/*
+result.appendChild(criaItem("li", textContent, receitas.ingredientes))
+result.appendChild(criaItem("p", textContent, receitas.preparo))
+/*
+
+})
